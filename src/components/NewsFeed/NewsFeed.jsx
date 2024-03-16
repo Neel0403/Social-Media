f44856b02418c928d8c605028a9ef165f1442691
Nidhi <neelo2003@gmail.com>
2024-03-16 18:31:52 +0530

    Added upload post component

--- src/components/NewsFeed/NewsFeed.jsx
+++ src/components/NewsFeed/NewsFeed.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import UploadPost from '../UploadPost/UploadPost'
 
 const NewsFeed = () => {
   return (
-    <div style={{flex:5.5}} className=''>NewsFeed</div>
+    <div style={{flex:5.5}} className=''>
+  <UploadPost />    
+    </div>
   )
 }
 
