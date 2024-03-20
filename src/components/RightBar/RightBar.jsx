1107c129c19ef04f21e2b28788215c5ee30fab73
Nidhi <neelo2003@gmail.com>
2024-03-20 23:26:12 +0530

    Updated Right Bar

--- src/components/RightBar/RightBar.jsx
+++ src/components/RightBar/RightBar.jsx
@@ -5,7 +5,7 @@ import profilePic from "../../assets/profilePic.png";
 
 const RightBar = () => {
   return (
-    <div style={{ flex: 3.5 }} className="">
+    <div style={{ flex: 3.5, zIndex: -1 }} className="">
       <div className="pt-[20px] pr-[20px]">
         <div className="flex items-center">
           <img
@@ -26,7 +26,11 @@ const RightBar = () => {
         <ul className="m-0 p-0">
           <li className="flex items-center mb-[10px]">
             <div className="mr-[10px] relative">
-              <img src={profilePic} alt="Profile Picture" className="w-[40px] h-[40px] object-cover rounded-full"/>
+              <img
+                src={profilePic}
+                alt="Profile Picture"
+                className="w-[40px] h-[40px] object-cover rounded-full"
+              />
               <span className="w-[12px] h-[12px] rounded-full bg-green-500 absolute top-[-1px] right-0 border-[2px] border-white"></span>
             </div>
             <span className="font-bold">ABC XYZ</span>
