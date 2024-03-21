0d9bb58b29d9f40727245ef80780dacabb94b395
Nidhi <neelo2003@gmail.com>
2024-03-21 22:35:08 +0530

    Updated Like Feature

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { MdOutlineMoreVert } from "react-icons/md";
-import profilePic from "../../assets/profilePic.png";
-import postPic from "../../assets/postPic.jpg";
 import likeIcon from "../../assets/like.png";
 import heartIcon from "../../assets/heart.png";
 import { Users } from "../../data/dummyData";
 
 const Post = ({ post }) => {
+  const [like, setLike] = useState(post.like);
+  const [isLiked, setIsLiked] = useState(false);
+
+  const handleLike = () => {
+    setLike(isLiked ? like - 1 : like + 1);
+    setIsLiked(!isLiked);
+  };
   return (
     <div className="w-full rounded-md shadow-lg mt-[30px] mb-[30px] p-[10px]">
       <div className="p-[10px]">
@@ -41,9 +46,19 @@ const Post = ({ post }) => {
       </div>
       <div className="flex items-center justify-between">
         <div className="flex items-center gap-[5px]">
-          <img src={likeIcon} alt="likeIcon" className="w-[24px] h-[24px]" />
-          <img src={heartIcon} alt="heartIcon" className="w-[24px] h-[24px]" />
-          <span className="text-sm">{post.like}</span>
+          <img
+            src={likeIcon}
+            alt="likeIcon"
+            className="w-[24px] h-[24px] cursor-pointer"
+            onClick={handleLike}
+          />
+          <img
+            src={heartIcon}
+            alt="heartIcon"
+            className="w-[24px] h-[24px] cursor-pointer"
+            onClick={handleLike}
+          />
+          <span className="text-sm">{like} likes</span>
         </div>
         <div>
           <span className="cursor-pointer border-b-[1px] border-slate-300 text-sm">
