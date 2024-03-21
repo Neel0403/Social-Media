fd977cfd011dcef86d8de22324d8f13e11471fcf
Nidhi <neelo2003@gmail.com>
2024-03-21 23:18:27 +0530

    Updated Right Bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
 import Home from './pages/Home'
+import { Route,Routes } from 'react-router-dom'
+import Profile from './pages/Profile'
 
 function App() {
 
   return (
-    <Home />
+    <Routes>
+      <Route path='/' element={<Profile />} /> 
+      {/* <Route path='/profile' element={<Profile />} />   */}
+    </Routes>
   )
 }
 
--- src/components/RightBar/RightBar.jsx
+++ src/components/RightBar/RightBar.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import birthDayIcon from "../../assets/gift.png";
-import adImage from "../../assets/ad.jpg";
 import profilePic from "../../assets/profilePic.png";
+import adImage from "../../assets/ad.jpg";
 import OnlineUsers from "../OnlineUsers/OnlineUsers";
 import { Users } from "../../data/dummyData";
 
 const RightBar = () => {
-  return (
-    <div style={{ flex: 3.5, zIndex: -1 }} className="">
-      <div className="pt-[20px] pr-[20px]">
+  const RightBarHome = () => {
+    return (
+      <>
         <div className="flex items-center">
           <img
             src={birthDayIcon}
@@ -30,6 +30,92 @@ const RightBar = () => {
             <OnlineUsers key={user.id} user={user} />
           ))}
         </ul>
+      </>
+    );
+  };
+
+  const RightBarProfile = () => {
+    return (
+      <>
+        <h1 className="font-bold text-xl mb-[10px]">User Information</h1>
+        <div className="mb-[30px]">
+          <div className="mb-[10px]">
+            <span className="font-semibold mr-[15px] text-slate-500">
+              City:
+            </span>
+            <span>Mumbai</span>
+          </div>
+          <div className="mb-[10px]">
+            <span className="font-semibold mr-[15px] text-slate-500">
+              From:
+            </span>
+            <span>Mumbai</span>
+          </div>
+          <div className="mb-[10px]">
+            <span className="font-semibold mr-[15px] text-slate-500">
+              Relationship:
+            </span>
+            <span>Single</span>
+          </div>
+          <h1 className="font-bold text-xl mb-[10px]">Friends</h1>
+          <div className="grid grid-cols-3 gap-4">
+            <div className="">
+              <img
+                src={profilePic}
+                alt="User Picture"
+                className="w-[100px] h-[100px] object-cover rounded-md"
+              />
+              <span>Derrick Kello</span>
+            </div>
+            <div>
+              <img
+                src={profilePic}
+                alt="User Picture"
+                className="w-[100px] h-[100px] object-cover rounded-md"
+              />
+              <span>Derrick Kello</span>
+            </div>
+            <div>
+              <img
+                src={profilePic}
+                alt="User Picture"
+                className="w-[100px] h-[100px] object-cover rounded-md"
+              />
+              <span>Derrick Kello</span>
+            </div>
+            <div>
+              <img
+                src={profilePic}
+                alt="User Picture"
+                className="w-[100px] h-[100px] object-cover rounded-md"
+              />
+              <span>Derrick Kello</span>
+            </div>
+            <div>
+              <img
+                src={profilePic}
+                alt="User Picture"
+                className="w-[100px] h-[100px] object-cover rounded-md"
+              />
+              <span>Derrick Kello</span>
+            </div>
+            <div>
+              <img
+                src={profilePic}
+                alt="User Picture"
+                className="w-[100px] h-[100px] object-cover rounded-md"
+              />
+              <span>Derrick Kello</span>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  };
+  return (
+    <div style={{ flex: 3.5, zIndex: -1 }} className="">
+      <div className="pt-[20px] pr-[20px]">
+        <RightBarProfile />
       </div>
     </div>
   );
