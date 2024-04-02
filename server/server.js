fc87ee27a3bb233c1dff9963ffa17f111f76993f
Nidhi <neelo2003@gmail.com>
2024-04-02 18:10:59 +0530

    Update Server

--- server/server.js
+++ server/server.js
@@ -4,19 +4,19 @@ import helmet from "helmet";
 import morgan from "morgan";
 import { dbConnect } from "./dbConnect/dbConnect.js";
 import cors from "cors";
-import routes from "./routes/routes.js"
+import routes from "./routes/routes.js";
 
 const app = express();
 dotenv.config();
 
 app.use(helmet());
-app.use(morgan("combined"));
+app.use(morgan("common"));
 app.use(cors());
 app.use(express.json());
 
-app.use(routes)
+app.use(routes);
 
-app.listen(5000, () => {
-  console.log("Server is running");
+app.listen(process.env.PORT || 8000, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
   dbConnect();
 });
